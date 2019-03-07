046f49f8a4f2f831b0e635201c8ceaa2c6b664f4
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2019-03-07 15:41:51 -0500

    Deployment updates

--- Website/install.sh
+++ Website/install.sh
@@ -4,5 +4,8 @@
 rm -r ~/public_html/takestock
 cp -r takestock ~/public_html/takestock
 
+# Setup the config files
+rm ~/public_html/takestock/config.php*
+rm ~/public_html/takestock/include/db.php*
 cp takestock/config.php.afs ~/public_html/takestock/config.php
 cp takestock/include/db.php.afs ~/public_html/takestock/include/db.php
