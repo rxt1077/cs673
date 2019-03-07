e8d048500ef6a94f9e624a52f851b2415d1b8ac7
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2019-03-07 15:37:47 -0500

    Deployment issues

--- Website/install.sh
+++ Website/install.sh
@@ -4,5 +4,5 @@
 rm -r ~/public_html/takestock
 cp -r takestock ~/public_html/takestock
 
-cp takestock/Website/config.php.afs ~/public_html/takestock/config.php
-cp takestock/Website/include/db.php.afs ~/public_html/takestock/include/db.php
+cp takestock/config.php.afs ~/public_html/takestock/config.php
+cp takestock/include/db.php.afs ~/public_html/takestock/include/db.php
