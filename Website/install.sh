8a7e5dfd5951b5120f883d70c0463d28350e97d3
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2019-03-07 15:36:18 -0500

    Deployment issues

--- Website/install.sh
+++ Website/install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Deploys website on AFS
-
 rm -r ~/public_html/takestock
 cp -r takestock ~/public_html/takestock
 
-sed "s/.*\$servername.*=.*/\$servername = 'sql.njit.edu';/" takestock/include/db.php > ~/public_html/takestock/include/db.php
-sed "s/.*\$basedir.*=.*/    \$basdir = '\/~rxt1077\/takestock';/" takestock/config.php > ~/public_html/takestock/config.php
+cp takestock/Website/config.php.afs ~/public_html/takestock/config.php
+cp takestock/Website/include/db.php.afs ~/public_html/takestock/include/db.php
