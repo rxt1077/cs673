7b43d7212011fc782b827d86de6ddba36f7c07e5
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2019-04-30 19:34:40 -0400

    Updated balance.R to output a CSV orders file

--- Runner/scripts/balance.R
+++ Runner/scripts/balance.R
@@ -1,104 +1,146 @@
 # This script creates an orders file that will balance a portfolio
+# It uses lpSolve repeatedly with smaller and smaller thresholds to find a
+# solution that creates a portfolio value closest to the original value. It
+# WILL NOT sell all of a stock.
+#
+# Usage is: Rscript balance.R <input> <output>
 
 library(lpSolve)
 
-args = commandArgs(trailingOnly=TRUE)
-input = args[1]
-output = args[2]
+# Adds values to a portfolio dataframe
+addValues <- function(stocks) {
+    stocks$value <- stocks$shares * stocks$price
+    stocks
+}
+
+# Calculates the totals for a portfolio dataframe
+calcTotals <- function(stocks) {
+    totals <- list(    
+        "all" = sum(stocks$value),
+        "nse" = sum(stocks$value[stocks$market == "NSE"]),
+        "dow" = sum(stocks$value[stocks$market == "DOW"]),
+        "nseRatio" = 0.00, 
+        "dowRatio" = 0.00
+    )
+    totals$nseRatio <- totals$nse / totals$all
+    totals$dowRatio <- totals$dow / totals$all
+    totals
+}
+
+# Prints out the portfolio
+printStocks <- function(stocks, totals, title) {
+    print(title)
+    print(sprintf("Total Value: $%0.2f", totals$all))
+    print(sprintf("Ratios: %0.2f%% DOW and %0.2f%% NSE", totals$dowRatio, totals$nseRatio))
+    print(stocks)
+}
+
+# Parse the arguments
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) != 2) {
+    stop("Invalid amount of arguments. Usage is Rscript balance.R <input> <output>")
+}
+input <- args[1]
+output <- args[2]
 
 # Read the input
-stocks = read.csv(file=input)
+stocks <- read.csv(file=input)
 
 # Determine the market for each stock
-stocks$market = ifelse(endsWith(toupper(stocks$symbol), ".NS"), "NSE", "DOW")
+stocks$market <- ifelse(endsWith(toupper(stocks$symbol), ".NS"), "NSE", "DOW")
 if ((! "DOW" %in% stocks$market) | (! "NSE" %in% stocks$market)) {
     stop("Portfolio does not contain at least one stock from each market.")
 }
 
-# Calculate the values
-stocks$value = stocks$shares * stocks$price
+# Add the values
+stocks <- addValues(stocks)
 
-# Calculate the total values and percentages
-totalValue = sum(stocks$value)
-nseTotalValue = sum(stocks$value[stocks$market == "NSE"])
-dowTotalValue = sum(stocks$value[stocks$market == "DOW"])
-nsePercentage = (nseTotalValue / totalValue) * 100
-dowPercentage = (dowTotalValue / totalValue) * 100
+# Calculate the totals
+totals <- calcTotals(stocks)
 
-print("Current Portfolio")
-print(sprintf("Total Value: $%0.2f", totalValue))
-print(sprintf("%0.2f%% DOW and %0.2f%% NSE", dowPercentage, nsePercentage))
-print(stocks)
+printStocks(stocks, totals, "Input Portfolio")
 
 # Balancing Variables
-nse = 0.30
-dow = 0.70
-thresh = 0.05 # The percentage rule is solved within +- this value
+nse <- 0.30
+dow <- 0.70
+thresh <- 0.05 # The percentage rule is solved within +- this value
 
 # NOT YET IMPLEMENTED
-change = 0.10 # The factor by which the amount of stock shares can change
+change <- 0.10 # The factor by which the amount of stock shares can change
 
 # Objective function
 
 # Maximize the value of the portfolio
-f.obj = stocks$price
+f.obj <- stocks$price
 
 # Constraints
 
 # the portfolio value cannot be great than its current value
-cons1 = stocks$price
+cons1 <- stocks$price
 
 # make vectors of the NSE / DOW coefficients to meet percentages
-cons2 = stocks$price
-cons2[stocks$market == "DOW"] = 0 
-cons3 = stocks$price
-cons3[stocks$market == "NSE"] = 0
+cons2 <- stocks$price
+cons2[stocks$market == "DOW"] <- 0 
+cons3 <- stocks$price
+cons3[stocks$market == "NSE"] <- 0
 
 # keep at least ONE of each stock
 # this tacks on an identity matrix to make sure each coefficient is > 0
-size = length(stocks$shares)
-cons4 = diag(size)
+size <- length(stocks$shares)
+cons4 <- diag(size)
 
-f.con = rbind(cons1, cons2, cons2, cons3, cons3, cons4)
-f.dir = c("<=", ">", "<", ">", "<", rep(">=", size))
+f.con <- rbind(cons1, cons2, cons2, cons3, cons3, cons4)
+f.dir <- c("<=", ">", "<", ">", "<", rep(">=", size))
 
 # Keep trying with smaller and smaller thresholds to find the best solution
-previous = NULL
+previous <- NULL
 while (thresh > 0.00) {
     print(sprintf("Trying to solve with a threshold of %0.2f", thresh))
-    f.rhs = c(
-        totalValue,
-        (nse - thresh) * totalValue,
-        (nse + thresh) * totalValue,
-        (dow - thresh) * totalValue,
-        (dow + thresh) * totalValue,
+    f.rhs <- c(
+        totals$all,
+        (nse - thresh) * totals$all,
+        (nse + thresh) * totals$all,
+        (dow - thresh) * totals$all,
+        (dow + thresh) * totals$all,
         rep(1, size)
     )
 
-    result = lp("max", f.obj, f.con, f.dir, f.rhs, int.vec=1:size)
+    result <- lp("max", f.obj, f.con, f.dir, f.rhs, int.vec=1:size)
     if (result$status != 0) {
         break
     }
-    previous = result
-    thresh = thresh - 0.01 
+    previous <- result
+    thresh <- thresh - 0.01 
 }
 
 if (is.null(previous)) {
     stop("Unable to balance portfolio.")
 }
 
-# Set the shares to the last solved coefficients
-stocks$shares = previous$solution
+# Setup the new portfolio
+newStocks <- stocks
+newStocks$shares <- previous$solution
+newStocks <- addValues(newStocks)
+newTotals <- calcTotals(newStocks)
 
-# Recalculate the values
-stocks$value = stocks$shares * stocks$price
-totalValue = sum(stocks$value)
-nseTotalValue = sum(stocks$value[stocks$market == "NSE"])
-dowTotalValue = sum(stocks$value[stocks$market == "DOW"])
-nsePercentage = (nseTotalValue / totalValue) * 100
-dowPercentage = (dowTotalValue / totalValue) * 100
+printStocks(newStocks, newTotals, "Output Portfolio")
 
-print("Balanced Portfolio")
-print(sprintf("Total Value: $%0.2f", totalValue))
-print(sprintf("%0.2f%% DOW and %0.2f%% NSE", dowPercentage, nsePercentage))
-print(stocks)
+# Create the order file
+orders = data.frame(
+    "difference" = newStocks$shares - stocks$shares,
+    "action" = rep("NONE", size),
+    "ticker" = newStocks$symbol,
+    "nshares" = rep(0, size),
+    stringsAsFactors = FALSE
+)
+orders$action[orders$difference > 0] <- "BUY" 
+orders$action[orders$difference < 0] <- "SELL" 
+orders$nshares <- abs(orders$difference)
+
+# Drop the difference column
+orders <- orders[c("action", "ticker", "nshares")]
+# Drop the NONE rows and SELL before we BUY (so we have money)
+orders <- rbind(orders[orders$action == "SELL", ],
+    orders[orders$action == "BUY", ])
+# Write the CSV
+write.csv(orders, file = output, row.names = FALSE)
