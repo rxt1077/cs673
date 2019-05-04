c5d3ae98477443a55b4ee7c4296f2916308213bf
v-patel18 <37550205+v-patel18@users.noreply.github.com>
2019-05-04 02:27:24 -0400

    Update beta.R

--- Runner/scripts/beta.R
+++ Runner/scripts/beta.R
@@ -16,7 +16,7 @@ calcBeta = function(historical, bseReturns, nyseReturns, symbol) {
     stockReturns <- calcReturns(historical = historical, symbol = symbol)
 
     # Determine which market returns to use
-    if (grepl(toupper(symbol), ".NS$")) {
+    if (".NS$", grepl(toupper(symbol))) {
         marketReturns <- bseReturns
     } else {
         marketReturns <- nyseReturns
